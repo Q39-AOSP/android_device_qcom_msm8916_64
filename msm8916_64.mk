fdba185a19cd2abf4e4e5a415674a74a1834d68e
Shashank Mittal <mittals@codeaurora.org>
2014-05-19 13:52:31 -0700

    msm8916_64: Initial commit to bring up 64bit userspace.
    Change-Id: Ib25a5bf13a9cc966efa983a51c265893c765204b

--- msm8916_64.mk
+++ msm8916_64.mk
@@ -1,4 +1,26 @@
-$(call inherit-product, device/qcom/msm8916_32/msm8916_32.mk)
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/qcom/common/common.mk)
 
 PRODUCT_NAME := msm8916_64
 PRODUCT_DEVICE := msm8916_64
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := MSM8916 for arm64
+
+# default is nosdcard, S/W button enabled in resource
+PRODUCT_CHARACTERISTICS := nosdcard
+
+#Android EGL implementation
+PRODUCT_PACKAGES += libGLES_android
