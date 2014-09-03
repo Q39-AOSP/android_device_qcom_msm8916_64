eed10ce5302c331b2f996d51fced04d98a6014ea
Srinivas Ramana <sramana@codeaurora.org>
2014-09-03 18:32:30 +0530

    device: qcom: msm8916_64: Add irq balance config file
    Add configuration file for IRQ balancer on 64-bit
    builds.
    
    Change-Id: I5c7d2fcb1be9569a9babb85256f79f9352573bc9

--- msm8916_64.mk
+++ msm8916_64.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += wcnss_service
 
+# MSM IRQ Balancer configuration file
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8916_64/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+
 #wlan driver
 PRODUCT_COPY_FILES += \
     device/qcom/msm8916_64/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
