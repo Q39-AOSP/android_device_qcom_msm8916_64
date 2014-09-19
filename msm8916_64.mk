21214f6932b5c1bf8cadd8b328bcd4eb0d56befb
Laxminath Kasam <lkasam@codeaurora.org>
2014-09-19 15:25:42 +0530

    msm8916_64: support aanc tuning mixer controls
    Add support for aanc tuning mixer controls for
    S, P and E paths.
    
    Change-Id: Iae10c4412a0e8bbf74fdc615a5177c26741909d3

--- msm8916_64.mk
+++ msm8916_64.mk
@@ -49,7 +49,8 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8916_32/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/qcom/msm8916_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     device/qcom/msm8916_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
-    device/qcom/msm8916_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+    device/qcom/msm8916_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    device/qcom/msm8916_64/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
 
 #ANT+ stack
