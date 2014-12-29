76d712b201e80a92275a7cfefb575c63808a67e5
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2014-12-29 14:51:29 +0530

    msm8916_64: Choose target specific media_codecs.xml
    - Add system property 'media.msm8939hw' to select appropriate
      media_codecs.xml
    - push all media_codecs.xml's into /system/etc/
    
    Change-Id: Idd2455373692543f606d657141aa22fde9707104
    CRs-Fixed: 770327

--- msm8916_64.mk
+++ msm8916_64.mk
@@ -7,7 +7,8 @@ TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 # media_profiles and media_codecs xmls for 8916
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += device/qcom/msm8916_32/media/media_profiles_8916.xml:system/etc/media_profiles.xml \
-                      device/qcom/msm8916_32/media/media_codecs_8916.xml:system/etc/media_codecs.xml
+                      device/qcom/msm8916_32/media/media_codecs_8916.xml:system/etc/media_codecs.xml \
+                      device/qcom/msm8916_32/media/media_codecs_8939.xml:system/etc/media_codecs_8939.xml
 endif
 
 TARGET_USES_QCA_NFC := other
