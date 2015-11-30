26654efc9cc531327d60e10f01febbe16e09ae0f
Paras Nagda <pnagda@codeaurora.org>
2015-11-30 19:52:51 +0530

    msm8939: Add entry for media_codecs_performance.xml
    - Copy 8939 specific media_codecs_performance.xml to
      system/etc folder
    
    Change-Id: If7951f5c8777cfd164e1f5ba2a005e1d7d865fea

--- msm8916_64.mk
+++ msm8916_64.mk
@@ -14,6 +14,7 @@ ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += device/qcom/msm8916_32/media/media_profiles_8916.xml:system/etc/media_profiles.xml \
                       device/qcom/msm8916_32/media/media_codecs_8916.xml:system/etc/media_codecs.xml \
                       device/qcom/msm8916_32/media/media_codecs_8939.xml:system/etc/media_codecs_8939.xml \
+                      device/qcom/msm8916_32/media/media_codecs_performance_8916_64_8939.xml:system/etc/media_codecs_performance_8939.xml \
                       device/qcom/msm8916_32/media/media_codecs_8929.xml:system/etc/media_codecs_8929.xml
 endif
 
