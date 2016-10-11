67f4d2c0be7115767595810ec305a928c0d520ae
c_yinbin <yinbinzhang@codeaurora.org>
2016-10-11 10:19:45 +0800

    msm8916_64: Add MULTI_LANG_ENGINE for Reverie font engine
    MULTI_LANG_ENGINE is used to enable Reverie font engine.
    MULTI_LANG_ZAWGYI is used for Myanmar Zawgyi, which cannot
    be supported at the same time for Myanmar Unicode.
    So disable MULTI_LANG_ZAWGYI by default.
    
    Change-Id: I366edced794e30aa6aa15ab0f474ac5be57a73a5
    CRs-Fixed: 1072247

--- msm8916_64.mk
+++ msm8916_64.mk
@@ -38,11 +38,11 @@ PRODUCT_MODEL := MSM8916 for arm64
 
 # When can normal compile this module,  need module owner enable below commands
 # font rendering engine feature switch
-#-include $(QCPATH)/common/config/rendering-engine.mk
-#ifneq (,$(strip $(wildcard $(PRODUCT_RENDERING_ENGINE_REVLIB))))
-#    MULTI_LANG_ENGINE := REVERIE
+-include $(QCPATH)/common/config/rendering-engine.mk
+ifneq (,$(strip $(wildcard $(PRODUCT_RENDERING_ENGINE_REVLIB))))
+     MULTI_LANG_ENGINE := REVERIE
 #    MULTI_LANG_ZAWGYI := REVERIE
-#endif
+endif
 
 
 PRODUCT_BOOT_JARS += qcom.fmradio
